bb4ebd37579e32a330d06e0028c8a78f9ee3f720
Aykhan Mammadov <amemmedov35@std.beu.edu.az>
2025-01-27 03:53:38 +0400

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 // API temel URL'i
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+export const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://ayxanmammadov.pythonanywhere.com/';
 
 // API endpoint'leri
 export const ENDPOINTS = {
